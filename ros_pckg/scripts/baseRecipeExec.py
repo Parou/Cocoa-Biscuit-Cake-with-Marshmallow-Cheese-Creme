e2bd400fac41281e5507d800162af108b8e8e0fb
ir4klis <ir4klis@gmail.com>
2018-05-15 13:10:35 +0300

    Machines will conquer the worls, lets have ROS functionality ready!

---
+++ ros_pckg/scripts/baseRecipeExec.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ROS ver. Kineic Kame / ubuntu 16.06LTS
+# Base execution node
+
+import rospy
+from std_msgs.msg import String
+
+def callback(data):
+	# this is for debug and will be muted 
+    rospy.loginfo("base baking step.: %s", data.data)
+    
+def base_exec():
+
+	# this is abstract now
+    # connectivity with debug functions is here gotta add HW spesc funcs
+	# obviously array is for debug and will be replaced	
+
+    rospy.init_node('base_recipe_executor')
+
+    rospy.Subscriber("base_recipe", String, callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    base_exec()
---
+++ ros_pckg/scripts/baseRecipeSend.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ROS ver. Kineic Kame / ubuntu 16.06LTS
+# Base execution node preparator
+
+import rospy
+from std_msgs.msg import String
+
+def base_recipe():
+	pub = rospy.Publisher('base_recipe', String, queue_size=3)
+	rospy.init_node('base_recipe_sender')
+	rate = rospy.Rate(5)
+
+
+	# this is abstract for now
+	# connectivity with debug functions is here gotta add HW spesc funcs
+	# array is for debug and will be replaced	
+
+	recipeArray=["Cocoa-Biscuit Cake with Marshmallow-Cheese Creme Ground:", "1 -  280g solid, dry biscuits violently dismembered.", "2 -  20g cocoa powder, dark as our souls.", "3 -  120g slowly and painfully molten butter.", "4 -  Force everything in a cake tin with a diameter of 26 cm and use a cup to press your victim tightly against it, leaving a border of maybe 1 cm that is also pressed against the sides.", "5 -  Put it in the fridge for 10 Minutes and take it out right before it freezes to death.", "6 -  Put it into the oven and torture your victim for 15 Minutes by 180C." ]
+	while not rospy.is_shutdown():
+		i = 0
+		step = 0
+		while step <= 6:
+			recipe_str = recipeArray[i]
+			i += 1
+			step += 1
+			# this is for debug and will be muted 
+			rospy.loginfo(recipe_str)
+			pub.publish(recipe_str)
+		
+		rate.sleep()
+
+if __name__ == '__main__':
+	try:
+		base_recipe()
+	except rospy.ROSInterruptException:
+		pass
