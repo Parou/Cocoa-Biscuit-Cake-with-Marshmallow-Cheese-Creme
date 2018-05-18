5ad2c7917bc380a76402e62bdf771513c96732c2
Skayo <jonasg.cool@gmail.com>
2018-05-18 22:59:48 +0200

    Forgot something...

--- php/remote-recipe.php
+++ php/remote-recipe.php
@@ -6,7 +6,7 @@ Website: https://alicepeters.de/
 */
 if(ini_get("allow_url_fopen") != 1) {
     ini_set("allow_url_fopen", 1);
-
+} 
 
 $recipe = 'https://raw.githubusercontent.com/Parou/Cocoa-Biscuit-Cake-with-Marshmallow-Cheese-Creme/master/html/basic-recipe.html';
 echo file_get_contents($recipe);
